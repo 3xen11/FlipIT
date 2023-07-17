713ed1f7fb757aa5763d53dd69ee6ea3eb0499df
Dawid Dardziński <dardzinski.d@gmail.com>
2023-07-17 16:25:11 +0200

    initialization

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="pl">
+      <Head>
+        <title>FiszkIT</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
