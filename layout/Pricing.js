86fa76f0f8bb183d5bcdea6b6e5772990d966589
Dawid Dardziński <dardzinski.d@gmail.com>
2023-07-21 21:51:38 +0200

    pricing [added]: changed styles for pricing card

--- layout/Pricing.js
+++ layout/Pricing.js
@@ -1,106 +1,86 @@
 import SectionHeading from '@/src/components/SectionHeading';
+import { BsPlusCircle } from 'react-icons/bs';
 
 const Pricing = () => {
   return (
     <section className="max-w-screen pt-20 bg-white pb-20">
       <SectionHeading heading={'Nie czekaj, zacznij naukę już teraz!'} />
-      <div className="max-w-7xl mx-auto p-5 grid grid-cols-12 gap-4 items-end">
+      <div className="max-w-7xl mx-auto p-5 grid grid-cols-12 gap-4  justify-around items-end">
         {/* 1 - NAJTAŃSZA OPCJA - START */}
-        <div className="h-[430px] col-start-2 col-span-3  flex flex-col  bg-teal-100 rounded-xl bg-opacity-70 hover:scale-105 transition-all shadow-lg">
-          <div className="bg-red-500 rounded-t-xl h-2/5"></div>
-          <div className="py-2 px-8 h-3/5 flex flex-col items-center">
-            <div className="h-4/5 w-full">
-              <ul className="mt-3 text-sm">
-                <li className="py-0.5 border-b-2  border-teal-200">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-200">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-200">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-200">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-200">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-200">
-                  lorem ipsum
-                </li>
-              </ul>
-            </div>
-            <button className="bg-teal-400 text-sm py-2 px-4 hover:text-teal-400 hover:bg-white active:scale-95 transition-all text-white">
-              Dodaj do koszyka
-            </button>
+        <div className="h-[450px] col-span-3 p-4  flex flex-col items-center relative bg-[url('/svg/pricingCardBackgroundLight.svg')] bg-cover bg-opacity-90 text-white hover:scale-105 transition-all shadow-lg">
+          <div className="bg-teal-800 h-14 flex justify-center px-8 items-center top-8 -left-8 absolute font-bold ">
+            <p className="tracking-widest">BASIC</p>
           </div>
+          <div className="flex  w-full mt-24 justify-around ">
+            <div className="h-16 w-16 active:scale-95 transition-all cursor-pointer flex items-center justify-center bg-teal-700">
+              <BsPlusCircle className="h-8 w-8 " />
+            </div>
+          </div>
+          <ul className="leading-8 mt-4 w-full tracking-wide">
+            <li>a. lorem ipsum dolor sit amet</li>
+            <li>b. lorem ipsum dolor sit amet</li>
+            <li>c. lorem ipsum dolor sit amet</li>
+            <li>d. lorem ipsum dolor sit amet</li>
+            <li>e. lorem ipsum dolor sit amet</li>
+          </ul>
+          <button className="mt-4 py-4 w-3/5 bg-teal-700 active:scale-95 transition-all">
+            Dodaj do koszyka
+          </button>
         </div>
         {/* 1 - NAJTAŃSZA OPCJA - KONIEC */}
 
         {/* 2 - NAJDROŻSZA OPCJA - START */}
-        <div className="h-[450px] col-span-4  flex flex-col  bg-teal-600 rounded-xl bg-opacity-90 text-white hover:scale-105 transition-all shadow-lg">
-          <div className="bg-red-500 rounded-t-xl h-2/5"></div>
-          <div className="py-2 px-8 h-3/5 flex flex-col items-center">
-            <div className="h-4/5 w-full">
-              <ul className="mt-3">
-                <li className="py-0.5 border-b-2  border-teal-600">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-600">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-600">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-600">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-600">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-600">
-                  lorem ipsum
-                </li>
-              </ul>
-            </div>
-            <button className="bg-teal-800 text-lg py-2 px-4 hover:text-teal-800 hover:bg-white active:scale-95 transition-all">
-              Dodaj do koszyka
-            </button>
+        <div className=" bg-[url('/svg/pricingCardBackgroundDark.svg')] bg-cover h-[450px] col-span-4 col-start-5 p-4  flex flex-col items-center relative bg-opacity-90 text-white hover:scale-105 transition-all shadow-lg">
+          <div className="bg-teal-500 h-14 flex justify-center px-8 items-center top-8 -left-8 absolute font-bold ">
+            <p className="tracking-widest">PREMIUM</p>
           </div>
+          <div className="flex  w-full mt-24 justify-around ">
+            <div className="h-16 w-16 active:scale-95 transition-all cursor-pointer flex items-center justify-center bg-teal-700">
+              <BsPlusCircle className="h-8 w-8 " />
+            </div>
+            <div className="h-16 w-16 active:scale-95 transition-all cursor-pointer flex items-center justify-center bg-teal-700">
+              <BsPlusCircle className="h-8 w-8 " />
+            </div>
+            <div className="h-16 w-16 active:scale-95 transition-all cursor-pointer flex items-center justify-center bg-teal-700">
+              <BsPlusCircle className="h-8 w-8 " />
+            </div>
+          </div>
+          <ul className="leading-8 mt-4 w-full tracking-wide">
+            <li>a. lorem ipsum dolor sit amet</li>
+            <li>b. lorem ipsum dolor sit amet</li>
+            <li>c. lorem ipsum dolor sit amet</li>
+            <li>d. lorem ipsum dolor sit amet</li>
+            <li>e. lorem ipsum dolor sit amet</li>
+          </ul>
+          <button className="mt-4 py-4 w-3/5 bg-teal-700 active:scale-95 transition-all">
+            Dodaj do koszyka
+          </button>
         </div>
         {/* 2 - NAJDROŻSZA OPCJA - KONIEC */}
 
         {/* 3 - ŚREDNIA OPCJA - START */}
-        <div className="h-[430px] col-span-3  flex flex-col  bg-teal-100 rounded-xl bg-opacity-70 hover:scale-105 transition-all shadow-lg">
-          <div className="bg-red-500 rounded-t-xl h-2/5"></div>
-          <div className="py-2 px-8 h-3/5 flex flex-col items-center">
-            <div className="h-4/5 w-full">
-              <ul className="mt-3 text-sm">
-                <li className="py-0.5 border-b-2  border-teal-200 ">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-200 ">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-200 ">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-200 ">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-200 ">
-                  lorem ipsum
-                </li>
-                <li className="py-0.5 border-b-2  border-teal-200 ">
-                  lorem ipsum
-                </li>
-              </ul>
-            </div>
-            <button className="bg-teal-400 text-sm py-2 px-4 hover:text-teal-400 hover:bg-white active:scale-95 transition-all text-white">
-              Dodaj do koszyka
-            </button>
+        <div className="h-[450px] col-span-3 col-start-10 p-4  flex flex-col items-center relative bg-[url('/svg/pricingCardBackgroundLight.svg')] bg-cover bg-opacity-90 text-white hover:scale-105 transition-all shadow-lg">
+          <div className="bg-teal-800 h-14 flex justify-center px-8 items-center top-8 -left-8 absolute font-bold ">
+            <p className="tracking-widest">STANDARD</p>
           </div>
+          <div className="flex  w-full mt-24 justify-around ">
+            <div className="h-16 w-16 active:scale-95 transition-all cursor-pointer flex items-center justify-center bg-teal-700">
+              <BsPlusCircle className="h-8 w-8 " />
+            </div>
+            <div className="h-16 w-16 active:scale-95 transition-all cursor-pointer flex items-center justify-center bg-teal-700">
+              <BsPlusCircle className="h-8 w-8 " />
+            </div>
+          </div>
+          <ul className="leading-8 mt-4 w-full tracking-wide">
+            <li>a. lorem ipsum dolor sit amet</li>
+            <li>b. lorem ipsum dolor sit amet</li>
+            <li>c. lorem ipsum dolor sit amet</li>
+            <li>d. lorem ipsum dolor sit amet</li>
+            <li>e. lorem ipsum dolor sit amet</li>
+          </ul>
+          <button className="mt-4 py-4 w-3/5 bg-teal-700 active:scale-95 transition-all">
+            Dodaj do koszyka
+          </button>
         </div>
         {/* 3 - ŚREDNIA OPCJA - KONIEC */}
       </div>
--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -15,10 +15,10 @@ const Card = ({ id, title, description }) => {
       onMouseLeave={handleCardFlip}
     >
       <div className="card-inner w-full h-full transition-transform duration-500">
-        <div className=" card-front absolute h-full w-full flex items-center justify-center text-center  rounded-xl shadow-lg flashcardBack text-white font-bold tracking-wide">
+        <div className=" card-front absolute h-full w-full flex items-center justify-center text-center  rounded-xl shadow-lg bg-[url('/svg/pricingCardBackground.svg')] bg-cover text-white font-bold tracking-wide">
           <p className="drop-shadow-lg">{title}</p>
         </div>
-        <div className="card-back p-4 leading-8 text-white absolute h-full w-full flex items-center justify-center rounded-xl shadow-lg -rotate-180 bg-[#042824]">
+        <div className="card-back p-4 leading-8 text-white absolute h-full w-full flex items-center justify-center rounded-xl shadow-lg -rotate-180 bg-teal-950">
           <p>{description}</p>
         </div>
       </div>
