f284f9ea5526b65c7854904e3ad324433ad3be09
Dawid Dardziński <dardzinski.d@gmail.com>
2023-07-27 17:22:39 +0200

    app flashcard [style]: added responsive card

--- layout/Application.js
+++ layout/Application.js
@@ -2,8 +2,8 @@ import AppFlashCard from '@/src/components/AppFlashCard';
 
 const Application = () => {
   return (
-    <section className="max-w-screen bg-slate-100 mt-10 p-20">
-      <div className="max-w-7xl flex justify-center mx-auto p-5 ">
+    <section className="max-w-screen bg-slate-100 mt-10 py-20">
+      <div className="max-w-7xl bg-red-500 w-full flex justify-center mx-auto p-5 ">
         <AppFlashCard />
       </div>
     </section>
--- src/components/AppFlashCard.js
+++ src/components/AppFlashCard.js
@@ -1,6 +1,6 @@
 const AppFlashCard = () => {
   return (
-    <div className=" bg-[url('/svg/pricingCardBackgroundDark.svg')] bg-cover relative w-3/5 max-w-xl h-80 rounded-xl bg-red-500 flex items-center justify-center overflow-hidden transition-all hover:shadow-lg hover:transform hover:-rotate-1 hover:scale-110">
+    <div className=" bg-[url('/svg/pricingCardBackgroundDark.svg')] bg-cover relative w-screen  sm:w-4/5 lg:w-3/5 max-w-xl h-80 rounded-xl  flex items-center justify-center overflow-hidden transition-all hover:shadow-lg hover:transform hover:-rotate-1 hover:scale-110">
       <p className="font-bold text-xl text-white">Pytanie</p>
       <div className="flex items-center absolute -rotate-45 w-full h-full p-5 box-border bg-white opacity-0 transition-all hover:opacity-100 hover:rotate-0">
         <p className="card__description mt-2.5 text-sm text-slate-700">
